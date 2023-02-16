eb09e0fa1805a37c7d4ca090fd546a8aacf52ea8
outsidethecode <outsidethecode@gmail.com>
2023-02-16 14:26:56 +0000

    initial commit

--- hardhat/scripts/update-state.js
+++ hardhat/scripts/update-state.js
@@ -3,55 +3,28 @@ const hre = require("hardhat");
 async function main() {
 
   // Import State contract from existing address
-  const contract = await hre.ethers.getContractAt("StateV2", "0xff8d57666B95E4f9560F4DB7412605A7B1d17918");
+  const contract = await hre.ethers.getContractAt("State", "0xdB4c661456A023a403CF83784f9d15a1D3540702");
 
   // Add inputs from proof
-  const id = "16902857626710129093777049632637234295525180659192520755330107189706293248"
-  const oldState = "8066878375842601856475243198843085975596912141564964024736389476326270909304"
-  const newState = "21115237637388129015117528037637357760051922988797104487501981291013305057525"
-  const isOldStateGenesis = "1"
+  const id = "0x00ca0b252b3f5ac5f13c9f675c297c5f2f043335dfc4d18c1265a5de49ab0000"
+  const oldState = "0x252b3f5ac5f13c9f675c297c5f2f043335dfc4d18c1265a5de49abbb3d0aaacd"
+  const newState = "0x07a64c7d4c5be707b9188cca29261d3fc89dc3086af0b229ff73f7dbcc47eb02"
+  const isOldStateGenesis = "0x0000000000000000000000000000000000000000000000000000000000000001"
 
-  const a = [
-    "1630421067897642627687294525861707117104539066333376262752291887227366059760",
-    "2740649345442919581954819090271932172601335600630355609761718067394798804404"
-  ]
-  
-  const b = [
-    [
-     "10030581716058119958124758248302905971642575891511562064813930982127193838979",
-     "10797080316543379766288890244734017902078199095917137620666224767776519275169"
-    ],
-    [
-     "19320023229863823341945472518807692384732716652585407280165417972017122537238",
-     "5593935735234647868873761806670296776501834052955717427835563593147545954783"
-    ]
-  ]
-
-  const c = [
-    "112117455385219068098203385689784799845678331923639369702057707867982316982",
-    "3035653997312817797202225901056025078071404743173839494883489552850461225375"
-  ]
-
-  console.log("Init ... ");
+  const a = ["0x162b69da7ee75d3f84b0ac04263c869ca022977ff7eea5524edd1f187bca6d2f", "0x00d845508f1530315c01f2eca0d46a1a063b0c85db428fcd220181f8008beb5e"]
+  const b = [["0x0cb4f4b0a0be51e36dc691b8bfe6502c1465ba32bef6b2e21fd35f0ccf1ccde8", "0x2f4000e5deaa4a10d76229414be393896a4f17056cf08c8ac0d1128384fe1d10"],["0x03762181a364bbbe6719620c7e878a817510b517232313a9bb9a558dce35c949", "0x2e396d764aa22357e4ff0d094dfde884f38a8d5e6b647d3a756c288a5066f426"]]
+  const c = ["0x168823b92ec61074a71fdb8db35739f1b6e1a0d8d330ef856751539e245ccdeb", "0x0026a45b87a02618917c787b9ee8ffe7cfa62234da1ca3d62525596346741b00"]
 
   // Check Identity State for your ID before state transition
-  let identityState0;
-  if (await contract.idExists(id)) {
-    console.log("ID exists");
-    identityState0 = await contract.getState(id);
-  } else {
-    console.log("ID does not exist");
-    identityState0 = 0;
-  }
+  let identityState0 = await contract.getState(id);
 
   console.log("Identity State at t=0", identityState0);
 
   // Execute state transition
   await contract.transitState(id, oldState, newState, isOldStateGenesis, a, b, c);
-  console.log("-----------------------------");
 
   // Check Identity State for your ID after state transition
-  identityState = await contract.getStateInfoById(id);
+  identityState = await contract.getState(id);
 
   console.log("Identity State at t=1", identityState);
 }
--- main.go
+++ main.go
@@ -1,239 +1,221 @@
 package main
 
 import (
-    "context"
+	"context"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"math/big"
 	"time"
-    "io/ioutil"
 
 	"github.com/iden3/go-circuits"
 	core "github.com/iden3/go-iden3-core"
 	"github.com/iden3/go-iden3-crypto/babyjub"
 	"github.com/iden3/go-iden3-crypto/poseidon"
-	merkletree "github.com/iden3/go-merkletree-sql/v2"
-	"github.com/iden3/go-merkletree-sql/v2/db/memory"
+	merkletree "github.com/iden3/go-merkletree-sql"
+	"github.com/iden3/go-merkletree-sql/db/memory"
 )
 
-// BabyJubJub key
 func main() {
 
-    // Identity Keypair -------------------------------------------------------------------------------
-    // generate babyJubjub private key randomly
-    babyJubjubPrivKey := babyjub.NewRandPrivKey()
+	// 1. BabyJubJub key
 
-    // generate public key from private key
-    babyJubjubPubKey := babyJubjubPrivKey.Public()
+	// generate babyJubjub private key randomly
+	babyJubjubPrivKey := babyjub.NewRandPrivKey()
 
-    // print public key
-    fmt.Println(babyJubjubPubKey)
+	// generate public key from private key
+	babyJubjubPubKey := babyJubjubPrivKey.Public()
 
-    // Proof of membership example ----------------------------------------------------------------------
-    ctx := context.Background()
+	// print public key
+	fmt.Println(babyJubjubPubKey)
 
-    // Tree storage
-    store := memory.NewMemoryStorage()
+	// 2. Sparse Merkle Tree
 
-    // Generate a new MerkleTree with 32 levels
-    mt, _ := merkletree.NewMerkleTree(ctx, store, 32)
+	ctx := context.Background()
 
-    // Add a leaf to the tree with index 1 and value 10
-    index1 := big.NewInt(1)
-    value1 := big.NewInt(10)
-    mt.Add(ctx, index1, value1)
+	// Tree storage
+	store := memory.NewMemoryStorage()
 
-    // Add another leaf to the tree
-    index2 := big.NewInt(2)
-    value2 := big.NewInt(15)
-    mt.Add(ctx, index2, value2)
+	// Generate a new MerkleTree with 32 levels
+	mt, _ := merkletree.NewMerkleTree(ctx, store, 32)
 
-    // Proof of membership of a leaf with index 1
-    proofExist, value, _ := mt.GenerateProof(ctx, index1, mt.Root())
+	// Add a leaf to the tree with index 1 and value 10
+	index1 := big.NewInt(1)
+	value1 := big.NewInt(10)
+	mt.Add(ctx, index1, value1)
 
-    fmt.Println("Proof of membership:", proofExist.Existence)
-    fmt.Println("Value corresponding to the queried index:", value)
+	// Add another leaf to the tree
+	index2 := big.NewInt(2)
+	value2 := big.NewInt(15)
+	mt.Add(ctx, index2, value2)
 
-    // Proof of non-membership of a leaf with index 4
-    proofNotExist, _, _ := mt.GenerateProof(ctx, big.NewInt(4), mt.Root())
+	// Proof of membership of a leaf with index 1
+	proofExist, value, _ := mt.GenerateProof(ctx, index1, mt.Root())
 
-    fmt.Println("Proof of membership:", proofNotExist.Existence)
+	fmt.Println("Proof of membership:", proofExist.Existence)
+	fmt.Println("Value corresponding to the queried index:", value)
 
-    // Generic claim example ----------------------------------------------------------------------
+	// Proof of non-membership of a leaf with index 4
+	proofNotExist, _, _ := mt.GenerateProof(ctx, big.NewInt(4), mt.Root())
 
-    // set claim expriation date to 2361-03-22T00:44:48+05:30
-    t := time.Date(2361, 3, 22, 0, 44, 48, 0, time.UTC)
+	fmt.Println("Proof of membership:", proofNotExist.Existence)
 
-    // set schema
-    ageSchema, _ := core.NewSchemaHashFromHex ("2e2d1c11ad3e500de68d7ce16a0a559e")  
+	// 3.1. Create a Generic Claim
 
-    // define data slots
-    birthday := big.NewInt(19960424)
-    documentType := big.NewInt(1)   
+	// set claim expriation date to 2361-03-22T00:44:48+05:30
+	t := time.Date(2361, 3, 22, 0, 44, 48, 0, time.UTC)
 
-    // set revocation nonce 
-    revocationNonce := uint64(1909830690)
+	// set schema
+	ageSchema, _ := core.NewSchemaHashFromHex("2e2d1c11ad3e500de68d7ce16a0a559e")
 
-    // set ID of the claim subject
-    id, _ := core.IDFromString("113TCVw5KMeMp99Qdvub9Mssfz7krL9jWNvbdB7Fd2")
+	// define data slots
+	birthday := big.NewInt(19960424)
+	documentType := big.NewInt(1)
 
-    // create claim 
-    claim, _ := core.NewClaim(ageSchema, core.WithExpirationDate(t), core.WithRevocationNonce(revocationNonce), core.WithIndexID(id), core.WithIndexDataInts(birthday, documentType))
+	// set revocation nonce
+	revocationNonce := uint64(1909830690)
 
-    // transform claim from bytes array to json 
-    claimToMarshal, _ := json.Marshal(claim)
+	// set ID of the claim subject
+	subjectId, _ := core.IDFromString("113TCVw5KMeMp99Qdvub9Mssfz7krL9jWNvbdB7Fd2")
 
-    fmt.Println(string(claimToMarshal))
+	// create claim
+	claim, _ := core.NewClaim(ageSchema, core.WithExpirationDate(t), core.WithRevocationNonce(revocationNonce), core.WithIndexID(subjectId), core.WithIndexDataInts(birthday, documentType))
 
+	// transform claim from bytes array to json
+	claimToMarshal, _ := json.Marshal(claim)
 
-    // Auth claim example ----------------------------------------------------------------------
+	fmt.Println(string(claimToMarshal))
 
-    authSchemaHash, _ := core.NewSchemaHashFromHex("ca938857241db9451ea329256b9c06e5")
+	// 3.2. Create Auth Claim
 
-    // Add revocation nonce. Used to invalidate the claim. This may be a random number in the real implementation.
-    revNonce := uint64(1)
+	authSchemaHash, _ := core.NewSchemaHashFromHex("ca938857241db9451ea329256b9c06e5")
 
-    // Create auth Claim 
-    authClaim, _ := core.NewClaim(authSchemaHash,
-    core.WithIndexDataInts(babyJubjubPubKey.X, babyJubjubPubKey.Y),
-    core.WithRevocationNonce(revNonce))
+	// Add revocation nonce. Used to invalidate the claim. This may be a random number in the real implementation.
+	revNonce := uint64(1)
 
-    authClaimToMarshal, _ := json.Marshal(authClaim)
+	authClaim, _ := core.NewClaim(authSchemaHash,
+		core.WithIndexDataInts(babyJubjubPubKey.X, babyJubjubPubKey.Y),
+		core.WithRevocationNonce(revNonce))
 
-    fmt.Println(string(authClaimToMarshal))
+	authClaimToMarshal, _ := json.Marshal(authClaim)
 
-    // Three identity trees example ----------------------------------------------------------------------
+	fmt.Println(string(authClaimToMarshal))
 
-    // Create empty Claims tree
-    clt, _ := merkletree.NewMerkleTree(ctx, memory.NewMemoryStorage(), 32) 
+	// 4.1. Generate identity trees
 
-    // Create empty Revocation tree
-    ret, _ := merkletree.NewMerkleTree(ctx, memory.NewMemoryStorage(), 32) 
+	// Create empty Claims tree
+	clt, _ := merkletree.NewMerkleTree(ctx, memory.NewMemoryStorage(), 32)
 
-    // Create empty Roots tree
-    rot, _ := merkletree.NewMerkleTree(ctx, memory.NewMemoryStorage(), 32) 
+	// Create empty Revocation tree
+	ret, _ := merkletree.NewMerkleTree(ctx, memory.NewMemoryStorage(), 32)
 
-    // Get the Index and the Value of the authClaim
-    hIndex, hValue, _ := authClaim.HiHv()
+	// Create empty Roots tree
+	rot, _ := merkletree.NewMerkleTree(ctx, memory.NewMemoryStorage(), 32)
 
-    // add auth claim to claims tree with value hValue at index hIndex
-    clt.Add(ctx, hIndex, hValue)
+	// Get the Index of the claim and the Value of the authClaim
+	hIndex, hValue, _ := authClaim.HiHv()
 
-    // print the roots
-    fmt.Println(clt.Root().BigInt(), ret.Root().BigInt(), rot.Root().BigInt())
+	// add auth claim to claims tree with value hValue at index hIndex
+	clt.Add(ctx, hIndex, hValue)
 
-    // Identity state example ----------------------------------------------------------------------
+	// print the roots
+	fmt.Println(clt.Root().BigInt(), ret.Root().BigInt(), rot.Root().BigInt())
 
-    // calculate Identity State as a hash of the three roots
-    state, _ := merkletree.HashElems(
-        clt.Root().BigInt(),
-        ret.Root().BigInt(),
-        rot.Root().BigInt())
+	// 4.2. Retrieve identity state
 
-    fmt.Println("Identity State:", state)
+	state, _ := merkletree.HashElems(
+		clt.Root().BigInt(),
+		ret.Root().BigInt(),
+		rot.Root().BigInt())
 
-    identityId, _ := core.IdGenesisFromIdenState(core.TypeDefault, state.BigInt())
+	fmt.Println("Identity State:", state)
 
-    fmt.Println("ID:", identityId)
+	// 4.3. Retrieve Identifier (ID)
 
-    // Signature Claim Issuance ----------------------------------------------------------------------
+	id, _ := core.IdGenesisFromIdenState(core.TypeDefault, state.BigInt())
 
-    // claimIndex, claimValue := claim.RawSlots()
-    // indexHash2, _ := poseidon.Hash(core.ElemBytesToInts(claimIndex[:]))
-    // valueHash2, _ := poseidon.Hash(core.ElemBytesToInts(claimValue[:]))
+	fmt.Println("ID:", id)
 
-    // // Poseidon Hash the indexHash and the valueHash together to get the claimHash
-    // claimHash, _ := merkletree.HashElems(indexHash2, valueHash2)
+	// 5. Issuing Claim by Signature
 
-    // // Sign the claimHash with the private key of the issuer
-    // claimSignature := babyJubjubPrivKey.SignPoseidon(claimHash.BigInt())
-    // fmt.Println("Claim Signature:", claimSignature)
+	// Retrieve indexHash and valueHash of the claim
+	claimIndex, claimValue := claim.RawSlots()
+	indexHash, _ := poseidon.Hash(core.ElemBytesToInts(claimIndex[:]))
+	valueHash, _ := poseidon.Hash(core.ElemBytesToInts(claimValue[:]))
 
-    // Signature via Mercle Tree ----------------------------------------------------------------------
+	// Poseidon Hash the indexHash and the valueHash together to get the claimHash
+	claimHash, _ := merkletree.HashElems(indexHash, valueHash)
 
-    // GENESIS STATE:
+	// Sign the claimHash with the private key of the issuer
+	claimSignature := babyJubjubPrivKey.SignPoseidon(claimHash.BigInt())
 
-    // 1. Generate Merkle Tree Proof for authClaim at Genesis State
-    authMTPProof, _, _ := clt.GenerateProof(ctx, hIndex, clt.Root())
-    authMTPProofToMarshal, _ := json.Marshal(authMTPProof)
-    fmt.Println("------ authMTPProof ------ ")
-    fmt.Println(string(authMTPProofToMarshal))
+	fmt.Println("Claim Signature:", claimSignature)
 
+	// 6. Issuing Claim by adding it to the Merkle Tree
 
-    
-    // 2. Generate the Non-Revocation Merkle tree proof for the authClaim at Genesis State
-    authNonRevMTPProof, _, _ := ret.GenerateProof(ctx, new(big.Int).SetUint64(revNonce), ret.Root())
+	// GENESIS STATE:
 
-    // Snapshot of the Genesis State
-    genesisTreeState := circuits.TreeState{
-        State:          state,
-        ClaimsRoot:     clt.Root(),
-        RevocationRoot: ret.Root(),
-        RootOfRoots:    rot.Root(),
-    }
-    // STATE 1:
+	// 1. Generate Merkle Tree Proof for authClaim at Genesis State
+	authMTPProof, _, _ := clt.GenerateProof(ctx, hIndex, clt.Root())
 
-    // Before updating the claims tree, add the claims tree root at Genesis state to the Roots tree.
-    rot.Add(ctx, clt.Root().BigInt(), big.NewInt(0))
+	// 2. Generate the Non-Revocation Merkle tree proof for the authClaim at Genesis State
+	authNonRevMTPProof, _, _ := ret.GenerateProof(ctx, new(big.Int).SetUint64(revNonce), ret.Root())
 
-    // Create a new random claim
-    schemaHex := hex.EncodeToString([]byte("myAge_test_claim"))
-    schema, _ := core.NewSchemaHashFromHex(schemaHex)
+	// Snapshot of the Genesis State
+	genesisTreeState := circuits.TreeState{
+		State:          state,
+		ClaimsRoot:     clt.Root(),
+		RevocationRoot: ret.Root(),
+		RootOfRoots:    rot.Root(),
+	}
+	// STATE 1:
 
-    code := big.NewInt(51)
+	// Before updating the claims tree, add the claims tree root at Genesis state to the Roots tree.
+	rot.Add(ctx, clt.Root().BigInt(), big.NewInt(0))
 
-    newClaim, _ := core.NewClaim(schema, core.WithIndexDataInts(code, nil))
+	// Create a new random claim
+	schemaHex := hex.EncodeToString([]byte("myAge_test_claim"))
+	schema, _ := core.NewSchemaHashFromHex(schemaHex)
 
-    // Get hash Index and hash Value of the new claim
-    hi, hv, _ := newClaim.HiHv()
+	code := big.NewInt(51)
 
-    // Add claim to the Claims tree
-    clt.Add(ctx, hi, hv)
+	newClaim, _ := core.NewClaim(schema, core.WithIndexDataInts(code, nil))
 
-    // Fetch the new Identity State
-    newState, _ := merkletree.HashElems(
-        clt.Root().BigInt(),
-        ret.Root().BigInt(),
-        rot.Root().BigInt())
+	// Get hash Index and hash Value of the new claim
+	hi, hv, _ := newClaim.HiHv()
 
-    newTreeState := circuits.TreeState{
-        State:          newState,
-        ClaimsRoot:     clt.Root(),
-        RevocationRoot: ret.Root(),
-        RootOfRoots:    rot.Root(),
-    }
-    
+	// Add claim to the Claims tree
+	clt.Add(ctx, hi, hv)
 
-    authNewStateMTPProof, _, _ := clt.GenerateProof(ctx, hi, nil)
+	// Fetch the new Identity State
+	newState, _ := merkletree.HashElems(
+		clt.Root().BigInt(),
+		ret.Root().BigInt(),
+		rot.Root().BigInt())
 
-    // Sign a message (hash of the genesis state + the new state) using your private key
-    hashOldAndNewStates, _ := poseidon.Hash([]*big.Int{state.BigInt(), newState.BigInt()})
+	// Sign a message (hash of the genesis state + the new state) using your private key
+	hashOldAndNewStates, _ := poseidon.Hash([]*big.Int{state.BigInt(), newState.BigInt()})
 
-    signature := babyJubjubPrivKey.SignPoseidon(hashOldAndNewStates)
+	signature := babyJubjubPrivKey.SignPoseidon(hashOldAndNewStates)
 
-    // Generate state transition inputs
-    stateTransitionInputs := circuits.StateTransitionInputs{
-        ID:                identityId,
-        OldTreeState:      genesisTreeState,
-        NewTreeState:      newTreeState,
-        IsOldStateGenesis: true,
-        AuthClaim: authClaim,
-        AuthClaimIncMtp: authMTPProof,
-        AuthClaimNonRevMtp: authNonRevMTPProof,
-        AuthClaimNewStateIncMtp: authNewStateMTPProof,
-        Signature: signature,
-    }
+	// Generate state transition inputs
+	stateTransitionInputs := circuits.StateTransitionInputs{
+		ID:                id,
+		OldTreeState:      genesisTreeState,
+		NewState:          newState,
+		IsOldStateGenesis: true,
+		AuthClaim: circuits.Claim{
+			Claim: authClaim,
+			Proof: authMTPProof,
+			NonRevProof: &circuits.ClaimNonRevStatus{
+				Proof: authNonRevMTPProof,
+			},
+		},
+		Signature: signature,
+	}
 
-    // Perform marshalling of the state transition inputs
-    inputBytes, _ := stateTransitionInputs.InputsMarshal()
+	// Perform marshalling of the state transition inputs
+	inputBytes, _ := stateTransitionInputs.InputsMarshal()
 
-    fmt.Println("string(inputBytes)")
-    fmt.Println(string(inputBytes))
-    fmt.Printf("%+v\n", stateTransitionInputs)
-
-    file, _ := json.Marshal(string(inputBytes))
- 
-	_ = ioutil.WriteFile("./compiled-circuits/stateTransition/stateTransition_js/input.json", file, 0644)
-
+	fmt.Println(string(inputBytes))
 }
